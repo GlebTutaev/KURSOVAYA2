44b7e1cd070027cbb10f48ac17844b7001506679
Глеб Тутаев <aka-laka-006@mail.ru>
2022-12-21 15:02:16 +0500

    DataBaseReady(1)

--- app/src/main/java/com/example/kursovaya/DbHelper.java
+++ app/src/main/java/com/example/kursovaya/DbHelper.java
@@ -1,30 +1,125 @@
 package com.example.kursovaya;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.sql.SQLException;
+
 public class DbHelper extends SQLiteOpenHelper {
 
 
-    public static final String DBNAME = "DataBase.db";
+    public static  String DB_NAME = "dataBase.db";
+    public static  String DB_PATH = "dataBase.db";
+    public static final int DB_VERSSION = 1;
+
+    private SQLiteDatabase mDataBase;
+    private final Context mContext;
+    private boolean mNeedUpdate = false;
 
 
     public DbHelper(Context context) {
-        super(context, "DataBase.db", null, 1);
+
+        super(context, DB_NAME, null, DB_VERSSION);
+        if (android.os.Build.VERSION.SDK_INT >= 17)
+            DB_PATH = context.getApplicationInfo().dataDir + "/databases/";
+        else
+            DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        this.mContext = context;
+
+        copyDataBase();
+
+        this.getReadableDatabase();
+    }
+
+    private void copyDataBase() {
+        if (!checkDataBase()) {
+            this.getReadableDatabase();
+            this.close();
+            try {
+                copyDBFile();
+            } catch (IOException exception) {
+                throw new Error("ErrorCopyingDataBase");
+            }
+        }
+    }
+
+    private void copyDBFile() throws IOException{
+        InputStream mInput = mContext.getAssets().open(DB_NAME);
+        OutputStream mOutput = new FileOutputStream(DB_PATH + DB_NAME);
+        byte[] mBuffer = new byte[1024];
+        int mLength;
+        while ((mLength = mInput.read(mBuffer)) > 0)
+            mOutput.write(mBuffer, 0, mLength);
+        mOutput.flush();
+        mOutput.close();
+        mInput.close();
+    }
+
+    private boolean checkDataBase() {
+        File dbFile = new File(DB_PATH + DB_NAME);
+        return dbFile.exists();
+    }
+
+    public void updateDataBase() throws IOException {
+        if (mNeedUpdate) {
+            File dbFile = new File(DB_PATH + DB_NAME);
+            if (dbFile.exists())
+                dbFile.delete();
+
+            copyDataBase();
+
+            mNeedUpdate = false;
+        }
+    }
+
+    public boolean openDataBase() throws SQLException {
+        mDataBase = SQLiteDatabase.openDatabase(DB_PATH + DB_NAME, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+        return mDataBase != null;
+    }
+
+    public synchronized void close() {
+        if (mDataBase != null)
+            mDataBase.close();
+        super.close();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE Roles ( ID   INTEGER PRIMARY KEY, Role TEXT )");
-        db.execSQL("CREATE TABLE Users( ID INTEGER PRIMARY KEY, Role REFERENCES Roles (ID), Login TEXT, Password TEXT)");
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)     {
 
+        if (i1 > i)
+            mNeedUpdate = true;
+    }
+
+    public Boolean checkusername(String Login){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from Users where Login=?", new String[] {Login});
+        if (cursor.getCount()>0)
+            return true;
+        return false;
+    }
+
+    public Boolean checkusernamepassword(String Login, String Password){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("select * from Users where Login=? and Password=?", new String[] {Login, Password});
+        if (cursor.getCount()>0)
+            return true;
+        return false;
     }
 }
--- app/src/main/java/com/example/kursovaya/MainActivity.java
+++ app/src/main/java/com/example/kursovaya/MainActivity.java
@@ -2,33 +2,77 @@ package com.example.kursovaya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText userName, password;
+    EditText userName, pass;
     Button signIn, guest;
+
+    private DbHelper dbHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        dbHelper = new DbHelper(this);
+        try {
+            dbHelper.updateDataBase();
+        } catch (IOException e) {
+            throw new Error("Oshibka");
+        }
+
+        try {
+            db = dbHelper.getWritableDatabase();
+
+        } catch (SQLException sqlException) {
+            throw sqlException;
+        }
+
         userName = findViewById(R.id.username);
-        password = findViewById(R.id.password);
+        pass = findViewById(R.id.password);
         signIn = findViewById(R.id.SignIn);
         guest = findViewById(R.id.guestClick);
 
         guest.setOnClickListener(view -> {
 
+            Intent intent = new Intent(this, GuestActivity.class);
+            startActivity(intent);
         });
 
         signIn.setOnClickListener(view -> {
 
+            String user = userName.getText().toString();
+            String password = pass.getText().toString();
+
+            if (TextUtils.isEmpty(user) || TextUtils.isEmpty(password)) {
+                Toast.makeText(this, "Not all fields are filled", Toast.LENGTH_SHORT).show();
+            } else {
+                Boolean checkuserpass = dbHelper.checkusernamepassword(user, password);
+                if (checkuserpass == true) {
+                    Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                } else {
+
+                    Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
-
 }
